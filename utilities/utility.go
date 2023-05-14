b1ece29e4415d7c30f8dce609db524b1366c0949
isophtalic <isophtalic@gmail.com>
2023-05-14 16:41:01 +0700

    generate key

--- utilities/utility.go
+++ utilities/utility.go
@@ -6,6 +6,7 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
+	"math/rand"
 	"os"
 )
 
@@ -75,4 +76,23 @@ func PEMStringToRSAPrivateKey(pemString []byte) interface{} {
 	}
 
 	return privateKey
+}
+func KeyGenerateString() (key string) {
+	for i := 0; i < 5; i++ {
+		link := ""
+		if i != 4 {
+			link = "-"
+		}
+		key += randStringRunes(5) + link
+	}
+	return
+}
+
+func randStringRunes(n int) string {
+	var letters = []rune("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
 }
